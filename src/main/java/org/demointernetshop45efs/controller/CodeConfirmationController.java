d6a9af99ddf2a2533128416149ffd77acf56c523
StanislavPB <perminovsb@gmail.com>
2025-04-30 20:01:08 +0100

    commit

--- src/main/java/org/demointernetshop45efs/controller/CodeConfirmationController.java
+++ src/main/java/org/demointernetshop45efs/controller/CodeConfirmationController.java
@@ -6,20 +6,27 @@ import org.demointernetshop45efs.service.ConfirmationCodeService;
 import org.demointernetshop45efs.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/code")
 public class CodeConfirmationController {
     private final UserService userService;
     private final ConfirmationCodeService confirmationCodeService;
 
-    public boolean codeRenew(String email){
+
+    @GetMapping("/renew")
+    public boolean codeRenew(@RequestParam String email){
         return userService.renewCode(email);
     };
 
 
-    public ResponseEntity<UserResponseDto> confirmationEmail(String codeConfirmation){
+    @GetMapping("/confirmation")
+    public ResponseEntity<UserResponseDto> confirmationEmail(@RequestParam String codeConfirmation){
         return new ResponseEntity<>(userService.confirmationEmail(codeConfirmation), HttpStatus.OK);
     }
 
---
+++ src/main/java/org/demointernetshop45efs/controller/UserController.java
@@ -0,0 +1,79 @@
+package org.demointernetshop45efs.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.demointernetshop45efs.dto.UserRequestDto;
+import org.demointernetshop45efs.dto.UserResponseDto;
+import org.demointernetshop45efs.dto.UserUpdateRequestDto;
+import org.demointernetshop45efs.entity.User;
+import org.demointernetshop45efs.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService service;
+    private final UserService userService;
+
+
+    //*  добавить нового пользователя
+    @PostMapping("/new")
+    public ResponseEntity<UserResponseDto> addNewUser(@RequestBody UserRequestDto request){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(service.registration(request));
+    };
+
+    //* найти всех пользователей (полная информация - для ADMIN)
+    @GetMapping("/full")
+    public ResponseEntity<List<User>> findAllFullDetails(){
+        return ResponseEntity.ok(service.findFullDetailUsers());
+    };
+
+    //* найти всех пользователей (ограниченная информация - для MANAGER)
+    @GetMapping("/manager/all")
+    public ResponseEntity<List<UserResponseDto>> findAll(){
+        return ResponseEntity.ok(service.findAllUsers());
+    };
+
+    //*найти пользователя по ID
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> findUserById(@PathVariable Integer id){
+        return ResponseEntity.ok(service.findUserById(id));
+    };
+
+    //*найти пользователя по email
+    @GetMapping()
+    public ResponseEntity<UserResponseDto> findUserByEmail(@RequestParam String email){
+        return ResponseEntity.ok(service.findUserByEmail(email));
+    };
+
+    // найти всех по имени
+//    @GetMapping("/name")
+//    public ResponseEntity<List<UserResponseDto>> findUserByLastName(String lastName){
+//        return ResponseEntity.ok(service.)
+//    };
+
+    // * обновить данные от имени пользователь (пользователь хочет
+    // поменять какие-то данные в своем профиле)
+    @PutMapping("/update")
+    public ResponseEntity<UserResponseDto> updateUser(@RequestBody UserUpdateRequestDto request){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(service.updateUser(request));
+    };
+
+
+    //* удаление записи
+    @DeleteMapping("/{id}")
+    public boolean deleteUser(@PathVariable Integer id){
+        return service.deleteUser(id);
+    };
+
+
+}
